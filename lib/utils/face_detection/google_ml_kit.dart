f2f4d1a87deb9a336f8d2358d235c83e3bfbf697
Rakhan Atthaya <rakhanatanox@gmail.com>
2025-11-12 10:26:46 +0700

    kamera

---
+++ lib/utils/face_detection/google_ml_kit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_3/utils/face_detection/vision.dart';
+
+// Face recognition using Google ML Kit
+class GoogleMlKit {
+  GoogleMlKit._();
+
+  static final Vision vision = Vision.instance;
+}
